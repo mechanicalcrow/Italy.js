110d021a1b5d8bae143bd9bbf879c6fa041bd2d0
Harry <boothbeetodd@gmail.com>
2021-12-03 02:24:47 -0600

    worked on product of all in array challenge

--- fundamentally-part2/object.js
+++ fundamentally-part2/object.js
@@ -2,19 +2,29 @@
 
 const domID = document.querySelector('#output');
 
-const Mannerly = {
-    fullName: "Mannerly Jones",
-    job: "prostitution",
-    hourlyRate: 56,
-    bankBalance: 16770,
-    birthYear: 2003,
-    calcAge: function (){
-     this.age = 2022 - this.birthYear;
-     return this.age;
-    }
+// const Mannerly = {
+//     fullName: "Mannerly Jones",
+//     job: "prostitution",
+//     hourlyRate: 56,
+//     bankBalance: 16770,
+//     birthYear: 2003,
+//     calcAge: function (){
+//      this.age = 2022 - this.birthYear;
+//      return this.age;
+//     }
+// }
+
+
+// //domID.innerHTML = `${Mannerly.fullName} is my name and ${Mannerly.job} at $${Mannerly.hourlyRate} hourly is my game.`;
+// Mannerly.calcAge();
+// domID.innerHTML = Mannerly.age;
+
+const arr = [1, 2, 3, 4, 5];
+
+let product = 1;
+
+for(let i = 0; i < arr.length; i++ ){
+    product = product * arr[i];
+    
 }
-
-
-//domID.innerHTML = `${Mannerly.fullName} is my name and ${Mannerly.job} at $${Mannerly.hourlyRate} hourly is my game.`;
-Mannerly.calcAge();
-domID.innerHTML = Mannerly.age;
+domID.innerHTML = `The product of the array ${arr} is simply ${product}. Pay up the $100, please.`;
