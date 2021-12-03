03ea4c50cc92a25f82c2a1b56d2c0edf0f3267dd
Harry <boothbeetodd@gmail.com>
2021-12-02 23:09:40 -0600

    more efficient object method

--- fundamentally-part2/object.js
+++ fundamentally-part2/object.js
@@ -8,11 +8,13 @@ const Mannerly = {
     hourlyRate: 56,
     bankBalance: 16770,
     birthYear: 2003,
-    calcAge: function (birthYear){
-      return  2022 - this.birthYear;
+    calcAge: function (){
+     this.age = 2022 - this.birthYear;
+     return this.age;
     }
 }
 
 
 //domID.innerHTML = `${Mannerly.fullName} is my name and ${Mannerly.job} at $${Mannerly.hourlyRate} hourly is my game.`;
-domID.innerHTML = Mannerly.calcAge();
+Mannerly.calcAge();
+domID.innerHTML = Mannerly.age;
