7a6bf58b0a890274e04a12d66e16e994da6dcbe0
Harry <boothbeetodd@gmail.com>
2021-12-06 19:06:01 -0600

    body mass objects

--- fundamentally-part2/object.js
+++ fundamentally-part2/object.js
@@ -1,21 +1,21 @@
 'use strict';
 
-const domID = document.querySelector('#output');
+ const domID = document.querySelector('#output');
 
-const Mannerly = {
-    fullName: "Mannerly Jones",
-    job: "prostitution",
-    hourlyRate: 56,
-    bankBalance: 16770,
-    birthYear: 2003,
-    calcAge: function (){
-     this.age = 2022 - this.birthYear;
-     return this.age;
-    }
-};
+// const Mannerly = {
+//     fullName: "Mannerly Jones",
+//     job: "prostitution",
+//     hourlyRate: 56,
+//     bankBalance: 16770,
+//     birthYear: 2003,
+//     calcAge: function (){
+//      this.age = 2022 - this.birthYear;
+//      return this.age;
+//     }
+// };
 
-Mannerly.calcAge();
-domID.innerHTML = `${Mannerly.fullName} is my name and ${Mannerly.job} at $${Mannerly.hourlyRate} hourly is my game. Oh, and I am ${Mannerly.age}`;
+// Mannerly.calcAge();
+// domID.innerHTML = `${Mannerly.fullName} is my name and ${Mannerly.job} at $${Mannerly.hourlyRate} hourly is my game. Oh, and I am ${Mannerly.age}`;
 //
  //domID.innerHTML = `${Mannerly.age}`;
 
@@ -28,3 +28,26 @@ domID.innerHTML = `${Mannerly.fullName} is my name and ${Mannerly.job} at $${Man
     
 // }
 // domID.innerHTML = `The product of the array ${arr} is simply ${product}. Pay up the $100, please.`;
+
+const Mark = {
+    fullName: "Mark Miller",
+    mass: 80,
+    height: 120,
+    calcBMI: function (){
+        this.bmi = this.mass / this.height;
+        return this.bmi;
+    }
+};
+
+const John = {
+    fullName: "John Smith",
+    mass: 100,
+    height: 160,
+    calcBMI: function (){
+        this.bmi = this.mass / this.height;
+        return this.bmi;
+    }
+};
+
+
+domID.innerHTML = `${John.fullName} has ${John.calcBMI()} on the body mass index. ${Mark.fullName} has ${Mark.calcBMI()}.`;
